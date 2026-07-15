d9beeccde6a4581163ca9de39c212cd1ef24fbc8
satyambuild <saynalbipul@gmail.com>
2026-07-15 17:47:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -67,6 +67,21 @@ current++;
 
 loadPage();
 
+  const noBtn = document.getElementById("noBtn");
+
+function moveButton() {
+
+    const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+    const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+
+    noBtn.style.position = "fixed";
+    noBtn.style.left = x + "px";
+    noBtn.style.top = y + "px";
+}
+
+noBtn.addEventListener("mouseover", moveButton);
+noBtn.addEventListener("touchstart", moveButton);
+
 }else{
 
 document.body.innerHTML=`
